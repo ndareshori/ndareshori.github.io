19199f1069af9677955e5a876241763f824ff2d8
Nick Dareshori <ndareshori@elon.edu>
2020-05-10 18:34:02 -0400

    added a bunch of extraneous(?) files

---
+++ moveSnake.js
@@ -0,0 +1,20 @@
+// var keyCode = "";
+//s = 83
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//     setInterval(sendSnake, 1000);
+// });
+
+window.onload = () => {
+    let elem = document.getElementById("snake");
+
+        elem.style.left = 100px;
+    }
+}
+//
+// function sendSnake() {
+//     var snake = document.getElementById("snake");
+//
+//     snake.style.left = 50px;
+//
+// }
