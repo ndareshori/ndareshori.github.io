a93fbe3abf55e7dd34f0f2b824b1f5b2d846cb25
Nick Dareshori <ndareshori@elon.edu>
2020-05-10 22:05:17 -0400

    testing swipes on moveSnake.js

--- moveSnake.js
+++ moveSnake.js
@@ -11,44 +11,57 @@ document.addEventListener('keyup', (e) => {
   }
 });
 
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
-var xDown = null;
-var yDown = null;
+document.addEventListener("touchstart", startTouch, false);
+document.addEventListener("touchmove", moveTouch, false);
 
-function handleTouchStart(evt) {
-    xDown = evt.touches[0].clientX;
-    yDown = evt.touches[0].clientY;
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+
+function startTouch(e) {
+  initialX = e.touches[0].clientX;
+  initialY = e.touches[0].clientY;
 };
 
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+function moveTouch(e) {
+  if (initialX === null) {
+    return;
+  }
 
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-        if ( xDiff > 0 ) {
-            sendSnake();
-        /* left swipe */
-        }
-        // else {
-        // /* right swipe */
-        // }
-     }
-     // else {
-    //     if ( yDiff > 0 ) {
-    //     /* up swipe */
-    //     } else {
-    //     /* down swipe */
-    //     }
-    // }
-    // /* reset values */
-    xDown = null;
-    yDown = null;
+  if (initialY === null) {
+    return;
+  }
+
+  var currentX = e.touches[0].clientX;
+  var currentY = e.touches[0].clientY;
+
+  var diffX = initialX - currentX;
+  var diffY = initialY - currentY;
+
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    // sliding horizontally
+    if (diffX > 0) {
+      // swiped left
+      alert("swiped left");
+    } else {
+      // swiped right
+      alert("swiped right");
+    }
+  } else {
+    // sliding vertically
+    if (diffY > 0) {
+      // swiped up
+      alert("swiped up");
+    } else {
+      // swiped down
+      alert("swiped down");
+    }
+  }
+
+  initialX = null;
+  initialY = null;
+
+  e.preventDefault();
 };
 
 
