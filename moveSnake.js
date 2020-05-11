c42f359f60a7a4d8c7b2fca818001361a046a45a
Nick Dareshori <ndareshori@elon.edu>
2020-05-10 21:33:24 -0400

    Added download button to index.html, added snakes to index.html

--- moveSnake.js
+++ moveSnake.js
@@ -1,20 +1,56 @@
 // var keyCode = "";
 //s = 83
 //
-// document.addEventListener('DOMContentLoaded', () => {
-//     setInterval(sendSnake, 1000);
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(sendSnake, 30000);
+});
 
-window.onload = () => {
-    let elem = document.getElementById("snake");
+document.addEventListener('keyup', (e) => {
+  if (e.keyCode === 83) {
+      sendSnake()
+  }
+});
 
-        elem.style.left = 100px;
-    }
-}
-//
-// function sendSnake() {
-//     var snake = document.getElementById("snake");
-//
-//     snake.style.left = 50px;
-//
+
+// let counter = 0;
+// function count() {
+//     counter++;
+//     c = document.getElementById("counter");
+//     c.innerHTML = counter;
 // }
+
+function sendSnake() {
+    let snake = document.createElement("img");
+    snake.src = "images/snake.png";
+    snake.alt = "snake";
+    let addTo = document.getElementById("snakeGround");
+    snake.style.top = ((Math.random() * screen.height - 100) + 120) + "px";
+    // addTo.style.top = "20px"
+    snake.style.height = '24px';
+    snake.style.with = '123px';
+    snake.style.position = "absolute";
+    snake.style.left = "-125px";
+    addTo.appendChild(snake);
+
+    let pos = -125;
+    let loop = setInterval(moveSnake, 2);
+    if (pos > screen.width + 125) {
+        clearInterval(loop);
+        addTo.removeChild(snake)
+    }
+
+    function moveSnake() {
+        snake.style.left = pos + "px";
+        pos += 2;
+    }
+    // while pos < 1000 {
+    //     snake.style.left = pos + "px";
+    //     pos++;
+    // }
+
+
+
+    // var snake = document.getElementById("snake");
+    //
+    // snake.style.left = 100 + "px";
+}
